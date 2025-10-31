a4e5e201eec25d2ff66e86b9dea56dc09faeae52
Rahamatunnisa Shaik <rahamatunnisa1121@gmail.com>
2025-10-31 22:32:09 +0530

    first program

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang-bookingapp
+
+go 1.25.3
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt" //format package
+
+func main(){
+	fmt.Println("Welcome to our conference booking application!");
+	fmt.Println("Get your tickets here to attend");
+	
+}
