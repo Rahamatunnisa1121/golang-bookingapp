750ddc29c23191f223543fc799d4522bd9b26cdd
Rahamatunnisa Shaik <rahamatunnisa1121@gmail.com>
2025-11-01 20:14:11 +0530

    Variables

--- main.go
+++ main.go
@@ -3,7 +3,18 @@ package main
 import "fmt" //format package
 
 func main(){
-	fmt.Println("Welcome to our conference booking application!");
-	fmt.Println("Get your tickets here to attend");
-	
+	conferenceName:= "GO CONFERENCE"
+	const conferenceTickets int =50
+	var remainingTickets uint =50
+	//fmt.Println("Welcome to", conferenceName,"booking application!")
+	//fmt.Printf("ConferenceName is of %T type\n",conferenceName)//string
+	// fmt.Printf("conferenceTickets is of %T type\n",conferenceTickets)//int
+	// fmt.Printf("remainingTickets is of %T type\n",remainingTickets)//uint
+	fmt.Printf("Welcome to %v booking application!\n",conferenceName)
+	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
+	fmt.Println("Get your tickets here to attend")
+	var (userName string; userTickets int)
+	userName="Ruhi"
+	userTickets=2
+	fmt.Printf("User %v booked %v tickets\n",userName,userTickets)
 }
